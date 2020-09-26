48bd136773b48648d16a87361facd197c1a89f5c
Ripunjay Singh <drakesandsymphony@gmail.com>
2020-09-26 17:38:56 +0000

    this is my fourth git commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,3 @@
 public class HelloWorld{
 	public static void main(String[] args){
-		System.out.println("I am taking CS407 because I want to learn about Linux and bash");
-	}
-}
-
+		System.out.println("My favorite food is Butter Chicken with a side of Naan");
