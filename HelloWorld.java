086a12dd1fae439df3a287d1f006f33d904b6ee0
Ripunjay Singh <drakesandsymphony@gmail.com>
2020-09-26 17:22:41 +0000

    this is my second git commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld{
 	public static void main(String[] args){
-		System.out.println("My name is Ripunjay Singh");
+		System.out.println("My favorite programming language is Python");
 	}
 }
 
